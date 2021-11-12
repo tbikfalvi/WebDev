6989a4a8f7ed58fff95fe49574045617c1da0042
Tamas Bikfalvi <tbikfalvi@gmail.com>
2021-11-12 21:29:29 +0100

    liszenszek a licence.php-ban

---
+++ download_bikfalvi_hu/belenus/official/licence.php
@@ -0,0 +1,194 @@
+<?php
+
+/*
++---------------------------------------------------------------+
+|	Belenus licence kulcsok
+|
+|	http://download.bikfalvi.hu/belenus/official/licence.php
+|
++---------------------------------------------------------------+
+*/
+
+session_start();
+
+$licences = '';
+$licences .= "BLNS01_DPKBTY<br>";
+$licences .= "BLNS02_GPXCLO<br>";
+$licences .= "BLNS03_BQTGPO<br>";
+$licences .= "BLNS04_ISOGKO<br>";
+$licences .= "BLNS05_HRXEQU<br>";
+$licences .= "BLNS06_ITTJSK<br>";
+$licences .= "BLNS07_FSAARU<br>";
+$licences .= "BLNS08_JNZBNT<br>";
+$licences .= "BLNS09_DMYDTT<br>";
+$licences .= "BLNS10_JNVIKX<br>";
+$licences .= "BLNS11_CPJFPT<br>";
+$licences .= "BLNS12_HSAIRJ<br>";
+$licences .= "BLNS13_JPOATZ<br>";
+$licences .= "BLNS14_BNYAPV<br>";
+$licences .= "BLNS15_BOVAMK<br>";
+$licences .= "BLNS16_EOZARO<br>";
+$licences .= "BLNS17_ESJCNX<br>";
+$licences .= "BLNS18_HKUBRJ<br>";
+$licences .= "BLNS19_HRKEPT<br>";
+$licences .= "BLNS20_ETTFSV<br>";
+$licences .= "BLNS21_FPKDKZ<br>";
+$licences .= "BLNS22_CPAITV<br>";
+$licences .= "BLNS23_EMACSU<br>";
+$licences .= "BLNS24_GTOAPA<br>";
+$licences .= "BLNS25_JNOCTZ<br>";
+$licences .= "BLNS26_DSJEKV<br>";
+$licences .= "BLNS27_FSUAOJ<br>";
+$licences .= "BLNS28_JKYJQK<br>";
+$licences .= "BLNS29_GSYALJ<br>";
+$licences .= "BLNS30_HQJATA<br>";
+$licences .= "BLNS31_ETKDMT<br>";
+$licences .= "BLNS32_IRXCNV<br>";
+$licences .= "BLNS33_CNYBSY<br>";
+$licences .= "BLNS34_CTKDRY<br>";
+$licences .= "BLNS35_BNZHTJ<br>";
+$licences .= "BLNS36_COJIRX<br>";
+$licences .= "BLNS37_FTAHTU<br>";
+$licences .= "BLNS38_EPZIRO<br>";
+$licences .= "BLNS39_GMJASU<br>";
+$licences .= "BLNS40_BRAGOX<br>";
+$licences .= "BLNS41_GPJHPY<br>";
+$licences .= "BLNS42_GRJBOY<br>";
+$licences .= "BLNS43_HRTHPU<br>";
+$licences .= "BLNS44_DKYCKT<br>";
+$licences .= "BLNS45_DLTBPY<br>";
+$licences .= "BLNS46_BNJFOK<br>";
+$licences .= "BLNS47_GKVCLJ<br>";
+$licences .= "BLNS48_CTXDMY<br>";
+$licences .= "BLNS49_DQXFSU<br>";
+$licences .= "BLNS50_CTTDNJ<br>";
+$licences .= "BLNS51_HQZELZ<br>";
+$licences .= "BLNS52_CSOCPK<br>";
+$licences .= "BLNS53_JOUBOU<br>";
+$licences .= "BLNS54_HMXHNJ<br>";
+$licences .= "BLNS55_CPXAKZ<br>";
+$licences .= "BLNS56_JKUIMT<br>";
+$licences .= "BLNS57_EQTEKV<br>";
+$licences .= "BLNS58_DSKFPT<br>";
+$licences .= "BLNS59_FKJCNV<br>";
+$licences .= "BLNS60_HTYHSU<br>";
+$licences .= "BLNS61_HNJETX<br>";
+$licences .= "BLNS62_ILXINJ<br>";
+$licences .= "BLNS63_JNTBQV<br>";
+$licences .= "BLNS64_BSZJRO<br>";
+$licences .= "BLNS65_HROIOX<br>";
+$licences .= "BLNS66_IQKCRU<br>";
+$licences .= "BLNS67_EPUAPX<br>";
+$licences .= "BLNS68_BKJDPJ<br>";
+$licences .= "BLNS69_GKJBQY<br>";
+$licences .= "BLNS70_FRYGOZ<br>";
+$licences .= "BLNS71_ETYHQU<br>";
+$licences .= "BLNS72_CTXEST<br>";
+$licences .= "BLNS73_COXDKU<br>";
+$licences .= "BLNS74_HTOCMJ<br>";
+$licences .= "BLNS75_JPVJQY<br>";
+$licences .= "BLNS76_GSXGNO<br>";
+$licences .= "BLNS77_IQXGNZ<br>";
+$licences .= "BLNS78_CNYENO<br>";
+$licences .= "BLNS79_BOUHSY<br>";
+$licences .= "BLNS80_COUERU<br>";
+$licences .= "BLNS81_ELAHLJ<br>";
+$licences .= "BLNS82_BLJFOV<br>";
+$licences .= "BLNS83_CROESK<br>";
+$licences .= "BLNS84_HSKGSK<br>";
+$licences .= "BLNS85_GOUIOZ<br>";
+$licences .= "BLNS86_JRVDNJ<br>";
+$licences .= "BLNS87_DRJHMT<br>";
+$licences .= "BLNS88_CPUCQY<br>";
+$licences .= "BLNS89_FPZHMJ<br>";
+$licences .= "BLNS90_BTVBNY<br>";
+$licences .= "BLNS91_HLYIQK<br>";
+$licences .= "BLNS92_GRXFKU<br>";
+$licences .= "BLNS93_CSUFMV<br>";
+$licences .= "BLNS94_ERXFNO<br>";
+$licences .= "BLNS95_DKXBMU<br>";
+$licences .= "BLNS96_JRXGRZ<br>";
+$licences .= "BLNS97_FSYDSO<br>";
+$licences .= "BLNS98_DQXCRK<br>";
+$licences .= "BLNS99_GOACKU<br>";
+$licences .= "BLNL01_JKXBQU<br>";
+$licences .= "BLNL02_CSKGTX<br>";
+$licences .= "BLNL03_CQXJQU<br>";
+$licences .= "BLNL04_FSTBQJ<br>";
+$licences .= "BLNL05_BTZCMJ<br>";
+$licences .= "BLNL06_BNKGMK<br>";
+$licences .= "BLNL07_CNACNJ<br>";
+$licences .= "BLNL08_GSOCKV<br>";
+$licences .= "BLNL09_DMOCPJ<br>";
+$licences .= "BLNL10_HKXFMU<br>";
+$licences .= "BLNL11_IPKFTK<br>";
+$licences .= "BLNL12_JMZFQU<br>";
+$licences .= "BLNL13_HPKFSZ<br>";
+$licences .= "BLNL14_IOTEKU<br>";
+$licences .= "BLNL15_DSVITJ<br>";
+$licences .= "BLNL16_DTOITO<br>";
+$licences .= "BLNL17_JTYILU<br>";
+$licences .= "BLNL18_DKVDTY<br>";
+$licences .= "BLNL19_CQAISU<br>";
+$licences .= "BLNL20_CPTJKU<br>";
+
+
+$head .= '
+<!DOCTYPE HTML PUBLIC "-//WebTechs//DTD Mozilla HTML 2.0//EN">
+<HTML>
+<HEAD>
+  <TITLE>Belenus licences</TITLE>
+  <META NAME="author" CONTENT="Bikfalvi Tamás">
+  <META NAME="description" CONTENT="Belenus licences">
+  <link rel="shortcut icon" href="/belenus.ico" />
+</HEAD>
+';
+
+$body_start = '
+<body>
+';
+
+$body = '';
+
+$body_end = '
+</body>
+';
+
+$foot = '
+</html>
+';
+
+
+if( isset($_GET['blns']) )
+{
+	if( isset($_GET['lal']) ) { fnShowPage( $GLOBALS['licences'] ); }
+	
+	if( strstr( $GLOBALS['licences'], $_GET['blns'] ) )
+	{
+		fnShowPage( "licence_found" );
+	}
+	else
+	{
+		fnShowPage( "licence_not_found" );
+	}
+}
+else
+{
+	header('Location: /index.html');
+}
+
+function fnShowPage( $text )
+{
+	$GLOBALS['body'] .= $text;
+	
+	echo $GLOBALS['head'];
+	
+	echo $GLOBALS['body_start'];
+	echo $GLOBALS['body'];
+	echo $GLOBALS['body_end'];
+	
+	echo $GLOBALS['foot'];
+}
+
+
+?>
